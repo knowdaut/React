994ee6bf7b9347c6acb4c443bea0350b5900db0e
knowdaut <nickdaut975@gmail.com>
2022-02-01 23:21:38 +0000

    Removed image slider/accordian/cards from project.

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -2,50 +2,31 @@
 import { Component } from 'react';
 import Navbar from "./Components/Navbar/Navbar";
 import { Jumbotron } from 'reactstrap';
-import Cards from "./Cards";
-import ImageSlider from './Components/ImageSlider';
-import { SliderData } from './Components/SliderData';
-import Accordian from './Components/Accordian';
 import './App.css';
 
 
 
 class App extends Component{
-  constructor(props){
-    super(props);
-    this.state={
-      fade1 : false,
-      fade2 : false
-    }
-  }
+  // constructor(props){
+  //   super(props);
+  //   this.state={
+  //     fade1 : false,
+  //     fade2 : false
+  //   }
+  // }
   render(){
     let cname = "lead";
     return(
       <div>
         <Navbar />
         <Jumbotron>
-          <h1 className = "display-3">My React App</h1>
-          <p className = {cname}> This is my first React App</p>
+          <h1 className = "display-3">Weather app stuff</h1>
+          <p className = {cname}> This is my first Weather App</p>
           <hr className = {"my-2"}/>
           <p className = {cname}></p>
         </Jumbotron>
-        <Cards 
-          fade1={this.state.fade1} 
-          cardOneState = {this.cardOneState}
-          fade2={this.state.fade2}
-          cardTwoState = {this.cardTwoState}
-
-        />
-        <ImageSlider slides={SliderData}/>
-        <Accordian/>
       </div>
     )
-  }
-  cardOneState(){
-    this.setState({fade1 : !this.state.fade1})
-  }
-  cardTwoState(){
-    this.setState({fade2 : !this.state.fade2})
   }
 }
 
--- react_app/src/Components/Navbar/Navbar.js
+++ react_app/src/Components/Navbar/Navbar.js
@@ -11,7 +11,7 @@ class Navbar extends Component{
     render(){
         return(
             <nav className="NavbarItems">
-                <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
+                <h1 className="navbar-logo">Weather App<i className="fab fa-react"></i></h1>
                 <div className="menu-icon" onClick={this.handleClick}>
                     <i className={ this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
                 </div>
