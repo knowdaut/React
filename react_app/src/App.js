110c406521c81ad8cd4c634a096faaca1519bc27
tylerkwallace <wallace_tyler1@columbusstate.edu>
2022-02-03 14:24:18 +0000

    centered, background gradient, clock added

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,14 +1,17 @@
 
 import { Component } from 'react';
-import Navbar from "./Components/Navbar/Navbar";
 import { Jumbotron } from 'reactstrap';
+import Clock from "./Clock";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import WebFont from 'webfontloader';
+
 
 let apikey = "af103c190cd36ff3f3fb1e0c135a2ee1";
 
 
-class App extends Component{
-  
+class App extends Component {
+
   // constructor(props){
   //   super(props);
   //   this.state={
@@ -16,17 +19,19 @@ class App extends Component{
   //     fade2 : false
   //   }
   // }
-  render(){
+  render() {
     let cname = "lead";
-    return(
-      <div>
-        <Navbar />
-        <Jumbotron>
-          <h1 className = "display-3">Weather app stuff</h1>
-          <p className = {cname}> This is my first Weather App</p>
-          <hr className = {"my-2"}/>
-          <p className = {cname}></p>
-        </Jumbotron>
+    return (
+      <div className='App'>
+        <div className='clock'>
+        <Clock format={'hh-mm'}/>
+        </div>
+        <div class="d-flex justify-content-md-center align-items-center vh-100">
+          <Jumbotron>
+            <h1 className="display-3">Weather app stuff</h1>
+            <p className={cname}> This is my first Weather App</p>
+          </Jumbotron>
+        </div>
       </div>
     )
   }
--- react_app/src/index.js
+++ react_app/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import WebFont from 'webfontloader';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +12,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
