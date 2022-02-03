4690c344ef71e06ece36a0880c659931210df6d7
knowdaut <nickdaut975@gmail.com>
2022-02-03 17:04:46 +0000

    working on setting lat and long up.

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,35 +1,59 @@
-
-import { Component } from 'react';
-import Navbar from "./Components/Navbar/Navbar";
-import { Jumbotron } from 'reactstrap';
+// import React, { useEffect, useState } from "react";
+// import { Component } from 'react';
+// import Navbar from "./Components/Navbar/Navbar";
+// import { Jumbotron } from 'reactstrap';
+// import './App.css';
 import './App.css';
+import React, { useEffect, useState } from "react";
 
-let apikey = "af103c190cd36ff3f3fb1e0c135a2ee1";
+export default function App() {
 
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
 
-class App extends Component{
-  
-  // constructor(props){
-  //   super(props);
-  //   this.state={
-  //     fade1 : false,
-  //     fade2 : false
-  //   }
-  // }
-  render(){
-    let cname = "lead";
-    return(
-      <div>
-        <Navbar />
-        <Jumbotron>
-          <h1 className = "display-3">Weather app stuff</h1>
-          <p className = {cname}> This is my first Weather App</p>
-          <hr className = {"my-2"}/>
-          <p className = {cname}></p>
-        </Jumbotron>
-      </div>
-    )
-  }
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude is:", lat)
+    console.log("Longitude is:", long)
+  }, [lat, long]);
+
+  console.log("Latitude is:", lat)
+  console.log("Longitude is:", long)
+
+  return (
+    <div className="App">
+      There is nothing here
+    </div>
+  );
 }
 
-export default App;
+// class App extends Component{
+  
+//   // constructor(props){
+//   //   super(props);
+//   //   this.state={
+//   //     fade1 : false,
+//   //     fade2 : false
+//   //   }
+//   // }
+//   render(){
+//     let cname = "lead";
+//     return(
+//       <div>
+//         <Navbar />
+//         <Jumbotron>
+//           <h1 className = "display-3">Weather app stuff</h1>
+//           <p className = {cname}> This is my first Weather App</p>
+//           <hr className = {"my-2"}/>
+//           <p className = {cname}></p>
+//         </Jumbotron>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
