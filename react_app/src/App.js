b7599d2a129d0b04d6fefbdd239a76b89b72a0f5
knowdaut <nickdaut975@gmail.com>
2022-02-16 00:17:28 +0000

    added stuff

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,9 +1,9 @@
-// import { Component } from 'react';
-// import Navbar from "./Components/Navbar/Navbar";
-// import { Jumbotron } from 'reactstrap';
+import { Component } from 'react';
+import Cards from "./Cards";
 // import './App.css';
 import './App.css';
 import React, { useState } from "react";
+import { render } from '@testing-library/react';
 
 const api = {
   key: "af103c190cd36ff3f3fb1e0c135a2ee1",
@@ -12,119 +12,138 @@ const api = {
 
 
 
+const App = (props) => {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     fade1: false,
+  //     fade2: false,
+  //     fade3: false,
+  //     fade4: false,
+  //     fade5: false,
+  //     fade6: false
 
-function App() {
-  const [query, setQuery] = useState('');
-  const [weather, setWeather] = useState({});
-
-  const search = evt =>{
-    if(evt.key === "Enter"){
-      fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
-          setWeather(result);
-          setQuery('');
-          console.log(result);
-        });          
-    }
-  }
-  // const [lat, setLat] = useState([]);
-  // const [long, setLong] = useState([]);
-  // const [data, setData] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     navigator.geolocation.getCurrentPosition(function(position) {
-  //       setLat(position.coords.latitude);
-  //       setLong(position.coords.longitude);
-  //     });
-
-  //     await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`)
-  //     .then(res => res.json())
-  //     .then(result => {
-  //       setData(result)
-  //       console.log(result);
-  //     });
   //   }
-  //   fetchData();
-  // }, [lat,long])
+  // } 
+  function ApiCall() {
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
   
-  const dateBuilder = (d) => {
-    let days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
-    let months = ["January","February","March","April","May","June","July",
-            "August","September","October","November","December"];
+    const search = evt =>{
+      if(evt.key === "Enter"){
+        fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+          .then(res => res.json())
+          .then(result => {
+            setWeather(result);
+            setQuery('');
+            console.log(result);
+          });          
+      }
+    }
     
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-    
-
-    return `${day} ${date} ${month} ${year}`
-    
-    
-  }
-  return (
-    <div className="App">
-      <main>
-        <div className='search-box'>
-          <input
-            type="text"
-            className="search-bar"
-            placeholder="Search..."
-            onChange = {e => setQuery(e.target.value)}
-            value = {query}
-            onKeyPress = {search}
-          />
-        </div>
-        {(typeof weather.main != "undefined") ? (
-          <div>
-            <div className='location-box'>
-              <div className='location'>{weather.name}, {weather.sys.country}</div>
-              <div className='date'>{dateBuilder(new Date())}</div>
-            </div>
-            <div className='temp'>
-              {(weather.main.temp)}
-            </div>
-            <div className='weather'>{weather.name}</div>
+    const dateBuilder = (d) => {
+      let days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+      let months = ["January","February","March","April","May","June","July",
+              "August","September","October","November","December"];
+      
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      
+  
+      return `${day} ${date} ${month} ${year}`
+      
+      
+    }
+  } 
+  // render(){
+    return (
+      <div className="App">
+        <main>
+          <div className='search-box'>
+            <input
+              type="text"
+              className="search-bar"
+              placeholder="Search..."
+              onChange = {e => setQuery(e.target.value)}
+              value = {query}
+              onKeyPress = {search}
+            />
           </div>
-        ) : ('')}
-      </main>
-    </div>
-  );
-}
-
-// function App(){
-//   console.log("Hello");
-// }
-
-
-
-
-// class App extends Component{
-  
-//   // constructor(props){
-//   //   super(props);
-//   //   this.state={
-//   //     fade1 : false,
-//   //     fade2 : false
-//   //   }
-//   // }
-//   render(){
-//     console.log('please work');
-//     let cname = "lead";
-//     return(
-//       <div>
-//         <Navbar />
-//         <Jumbotron>
-//           <h1 className = "display-3">Weather app stuff</h1>
-//           <p className = {cname}> This is my first Weather App</p>
-//           <hr className = {"my-2"}/>
-//           <p className = {cname}></p>
-//         </Jumbotron>
-//       </div>
-//     )
-//   }
+          {(typeof weather.main != "undefined") ? (
+            <div>
+              <div className='location-box'>
+                <div className='location'>{weather.name}, {weather.sys.country}</div>
+                <div className='date'>{dateBuilder(new Date())}</div>
+              </div>
+              <div className='temp'>
+                {(weather.main.temp)}
+              </div>
+              <div className='weather'>{weather.weather[0].main}</div>
+            </div>
+          ) : ('')}
+        </main>
+        <Cards
+          fade1={this.state.fade1}
+          cardOneState={this.cardOneState}
+          fade2={this.state.fade2}
+          cardTwoState={this.cardTwoState}
+          fade3={this.state.fade3}
+          cardThreeState={this.cardThreeState}
+          fade4={this.state.fade4}
+          cardFourState={this.cardFourState}
+          fade5={this.state.fade5}
+          cardFiveState={this.cardFiveState}
+          fade6={this.state.fade6}
+          cardSixState={this.cardSixState}
+        />
+      </div>
+    );
+  }
 // }
 
 export default App;
+
+// return (
+//   <div className="App">
+//     <main>
+//       <div className='search-box'>
+//         <input
+//           type="text"
+//           className="search-bar"
+//           placeholder="Search..."
+//           onChange = {e => setQuery(e.target.value)}
+//           value = {query}
+//           onKeyPress = {search}
+//         />
+//       </div>
+//       {(typeof weather.main != "undefined") ? (
+//         <div>
+//           <div className='location-box'>
+//             <div className='location'>{weather.name}, {weather.sys.country}</div>
+//             <div className='date'>{dateBuilder(new Date())}</div>
+//           </div>
+//           <div className='temp'>
+//             {(weather.main.temp)}
+//           </div>
+//           <div className='weather'>{weather.weather[0].main}</div>
+//         </div>
+//       ) : ('')}
+//     </main>
+//     <Cards
+//       fade1={this.state.fade1}
+//       cardOneState={this.cardOneState}
+//       fade2={this.state.fade2}
+//       cardTwoState={this.cardTwoState}
+//       fade3={this.state.fade3}
+//       cardThreeState={this.cardThreeState}
+//       fade4={this.state.fade4}
+//       cardFourState={this.cardFourState}
+//       fade5={this.state.fade5}
+//       cardFiveState={this.cardFiveState}
+//       fade6={this.state.fade6}
+//       cardSixState={this.cardSixState}
+//     />
+//   </div>
+// );
