7ccb07439beb460f443129e34a02e482a706c131
knowdaut <nickdaut975@gmail.com>
2022-02-18 02:58:06 +0000

    fixed cards

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import Cards from "./Cards";
+import Card from './Cards.js' 
 // import './App.css';
 import './App.css';
 import React, { useState } from "react";
@@ -12,138 +12,73 @@ const api = {
 
 
 
-const App = (props) => {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     fade1: false,
-  //     fade2: false,
-  //     fade3: false,
-  //     fade4: false,
-  //     fade5: false,
-  //     fade6: false
+function App() {
+  const [fade1, setFade1] = useState(false);
+  const [fade2, setFade2] = useState(false);
+  const [fade3, setFade3] = useState(false);
+  
 
-  //   }
-  // } 
-  function ApiCall() {
-    const [query, setQuery] = useState('');
-    const [weather, setWeather] = useState({});
-  
-    const search = evt =>{
-      if(evt.key === "Enter"){
-        fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
-          .then(res => res.json())
-          .then(result => {
-            setWeather(result);
-            setQuery('');
-            console.log(result);
-          });          
-      }
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt =>{
+    if(evt.key === "Enter"){
+      fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });          
     }
-    
-    const dateBuilder = (d) => {
-      let days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
-      let months = ["January","February","March","April","May","June","July",
-              "August","September","October","November","December"];
-      
-      let day = days[d.getDay()];
-      let date = d.getDate();
-      let month = months[d.getMonth()];
-      let year = d.getFullYear();
-      
-  
-      return `${day} ${date} ${month} ${year}`
-      
-      
-    }
-  } 
-  // render(){
-    return (
-      <div className="App">
-        <main>
-          <div className='search-box'>
-            <input
-              type="text"
-              className="search-bar"
-              placeholder="Search..."
-              onChange = {e => setQuery(e.target.value)}
-              value = {query}
-              onKeyPress = {search}
-            />
-          </div>
-          {(typeof weather.main != "undefined") ? (
-            <div>
-              <div className='location-box'>
-                <div className='location'>{weather.name}, {weather.sys.country}</div>
-                <div className='date'>{dateBuilder(new Date())}</div>
-              </div>
-              <div className='temp'>
-                {(weather.main.temp)}
-              </div>
-              <div className='weather'>{weather.weather[0].main}</div>
-            </div>
-          ) : ('')}
-        </main>
-        <Cards
-          fade1={this.state.fade1}
-          cardOneState={this.cardOneState}
-          fade2={this.state.fade2}
-          cardTwoState={this.cardTwoState}
-          fade3={this.state.fade3}
-          cardThreeState={this.cardThreeState}
-          fade4={this.state.fade4}
-          cardFourState={this.cardFourState}
-          fade5={this.state.fade5}
-          cardFiveState={this.cardFiveState}
-          fade6={this.state.fade6}
-          cardSixState={this.cardSixState}
-        />
-      </div>
-    );
   }
-// }
+  
+  const dateBuilder = (d) => {
+    let days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+    let months = ["January","February","March","April","May","June","July",
+            "August","September","October","November","December"];
+    
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    
+
+    return `${day} ${date} ${month} ${year}`
+    
+    
+  } 
+  return (
+    <div className="App">
+      <main>
+        <div className='search-box'>
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange = {e => setQuery(e.target.value)}
+            value = {query}
+            onKeyPress = {search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className='location-box'>
+              <div className='location'>{weather.name}, {weather.sys.country}</div>
+              <div className='date'>{dateBuilder(new Date())}</div>
+            </div>
+            <div className='temp'>
+              {(weather.main.temp)}
+            </div>
+            <div className='weather'>{weather.weather[0].main}</div>
+          </div>
+        ) : ('')}
+        <Card cardOneState={() => setFade1(!fade1)} cardTwoState={() => setFade2(!fade2)} cardThreeState={() => setFade3(!fade3)} fade1={fade1} fade2={fade2} fade3={fade3}/> 
+      </main>
+    </div>
+  );
+}
+
 
 export default App;
 
-// return (
-//   <div className="App">
-//     <main>
-//       <div className='search-box'>
-//         <input
-//           type="text"
-//           className="search-bar"
-//           placeholder="Search..."
-//           onChange = {e => setQuery(e.target.value)}
-//           value = {query}
-//           onKeyPress = {search}
-//         />
-//       </div>
-//       {(typeof weather.main != "undefined") ? (
-//         <div>
-//           <div className='location-box'>
-//             <div className='location'>{weather.name}, {weather.sys.country}</div>
-//             <div className='date'>{dateBuilder(new Date())}</div>
-//           </div>
-//           <div className='temp'>
-//             {(weather.main.temp)}
-//           </div>
-//           <div className='weather'>{weather.weather[0].main}</div>
-//         </div>
-//       ) : ('')}
-//     </main>
-//     <Cards
-//       fade1={this.state.fade1}
-//       cardOneState={this.cardOneState}
-//       fade2={this.state.fade2}
-//       cardTwoState={this.cardTwoState}
-//       fade3={this.state.fade3}
-//       cardThreeState={this.cardThreeState}
-//       fade4={this.state.fade4}
-//       cardFourState={this.cardFourState}
-//       fade5={this.state.fade5}
-//       cardFiveState={this.cardFiveState}
-//       fade6={this.state.fade6}
-//       cardSixState={this.cardSixState}
-//     />
-//   </div>
-// );
