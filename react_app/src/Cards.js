56a43460ac64f0e21d5804cd973da666efacf1a3
knowdaut <nickdaut975@gmail.com>
2022-03-10 23:58:01 +0000

    fixed loop issue, passed daily info to txtfrcards

--- react_app/src/Cards.js
+++ react_app/src/Cards.js
@@ -20,150 +20,42 @@ function dayConverter(dayNum){
 
 const Cards = (props) => {
     var cardStorage = []
-    // if(typeof props.weather2 != "undefined" && props.weather2.daily){
-    //     props.weather2.daily.slice(0,5).forEach(daily => {
-    //         cardStorage.push(
-    //         <Col>
-    //             <Card key="firstCard"
-    //                 tabIndex={0}
-    //                 onClick={() => props.cardOneState()}>
-    //                 <CardBody>
-    //                     <CardTitle>
-    //                         <center>{dayConverter(utcConverter(daily.dt))}</center>
-    //                         <span id="forecast-icon">
-    //                             <center><i className="fas fa-cloud fa-2x"></i></center>
-    //                         </span>
-    //                     </CardTitle>
-    //                     <div>
-    //                         <center><p>{props.weather2.daily.temp}</p></center>
-    //                     </div>
-    //                     <Fade in={props.fade1} className="my-2">
-    //                         <CardText>
-    //                             <TextForCards cardNum={1} />
-    //                         </CardText>
-    //                     </Fade>
-    //                 </CardBody>
-    //             </Card>
-    //         </Col>
-    //         )
-    //     })
-    // }
-    return (
-        <div>
-            <Container className="card-container">
-                <Row>
-                    {/* {cardStorage} */}
+    if(typeof props.weather2 != "undefined" && props.weather2.daily){
+        props.weather2.daily.forEach((daily, index) => {
+            if(index > 0 && index < 6){
+                cardStorage.push(
                     <Col>
                         <Card key="firstCard"
                             tabIndex={0}
                             onClick={() => props.cardOneState()}>
                             <CardBody>
                                 <CardTitle>
-                                    <center>{(typeof props.weather2 != "undefined") && props.weather2.current ? dayConverter(utcConverter(props.weather2.daily[1].dt)) : " error "}</center>
+                                    <center>{dayConverter(utcConverter(daily.dt))}</center>
                                     <span id="forecast-icon">
                                         <center><i className="fas fa-cloud fa-2x"></i></center>
-                                        {/* <center><img src={`http://openweathermap.org/img/w/${props.weather2.daily[0].weather.icon}.png`} /></center> */}
                                     </span>
                                 </CardTitle>
                                 <div>
-                                    <center><p>{Math.round(props.weather.main.temp)}&#176;F</p></center>
+                                    <center><p>{daily.temp.day}</p></center>
                                 </div>
                                 <Fade in={props.fade1} className="my-2">
                                     <CardText>
-                                        <TextForCards cardNum={1} props={props} />
+                                        <TextForCards cardNum={1} daily={daily}/>
                                     </CardText>
                                 </Fade>
                             </CardBody>
                         </Card>
                     </Col>
-                    <Col>
-                        <Card key="secondCard"
-                            tabIndex={0}
-                            onClick={() => props.cardTwoState()}>
-                            <CardBody>
-                                <CardTitle>
-                                    <center>{(typeof props.weather2 != "undefined") && props.weather2.current ? dayConverter(utcConverter(props.weather2.daily[2].dt)) : " error "}</center>
-                                    <span id="forecast-icon">
-                                        <center><i className="fas fa-cloud fa-2x"></i></center>
-                                    </span>
-                                </CardTitle>
-                                <div>
-                                    <center><p>{(typeof props.weather2 != "undefined") && props.weather2.current ? Math.round(props.weather2.daily[2].temp.day) : " error "}&#176;F</p></center>
-                                </div>
-                                <Fade in={props.fade2} className="my-2">
-                                    <CardText>
-                                        <TextForCards cardNum={2} props={props}/>
-                                    </CardText>
-                                </Fade>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card key="thirdCard"
-                            tabIndex={0}
-                            onClick={() => props.cardThreeState()}>
-                            <CardBody>
-                                <CardTitle>
-                                    <center>{(typeof props.weather2 != "undefined") && props.weather2.current ? dayConverter(utcConverter(props.weather2.daily[3].dt)) : " error "}</center>
-                                    <span id="forecast-icon">
-                                        <center><i className="fas fa-cloud fa-2x"></i></center>
-                                    </span>
-                                </CardTitle>
-                                <div>
-                                    <center><p>{(typeof props.weather2 != "undefined") && props.weather2.current ? Math.round(props.weather2.daily[3].temp.day) : " error "}&#176;F</p></center>
-                                </div>
-                                <Fade in={props.fade3} className="my-2">
-                                    <CardText>
-                                        <TextForCards cardNum={3} props={props}/>
-                                    </CardText>
-                                </Fade>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card key="fourthCard"
-                            tabIndex={0}
-                            onClick={() => props.cardFourState()}>
-                            <CardBody>
-                                <CardTitle>
-                                    <center>{(typeof props.weather2 != "undefined") && props.weather2.current ? dayConverter(utcConverter(props.weather2.daily[4].dt)) : " error "}</center>
-                                    <span id="forecast-icon">
-                                        <center><i className="fas fa-cloud fa-2x"></i></center>
-                                    </span>
-                                </CardTitle>
-                                <div>
-                                    <center><p>{(typeof props.weather2 != "undefined") && props.weather2.current ? Math.round(props.weather2.daily[4].temp.day) : " error "}&#176;F</p></center>
-                                </div>
-                                <Fade in={props.fade4} className="my-2">
-                                    <CardText>
-                                        <TextForCards cardNum={4} props={props}/>
-                                    </CardText>
-                                </Fade>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card key="fifthCard"
-                            tabIndex={0}
-                            onClick={() => props.cardFiveState()}>
-                            <CardBody>
-                                <CardTitle>
-                                    <center>{(typeof props.weather2 != "undefined") && props.weather2.current ? dayConverter(utcConverter(props.weather2.daily[5].dt)) : " error "}</center>
-                                    <span id="forecast-icon">
-                                        <center><i className="fas fa-cloud fa-2x"></i></center>
-                                    </span>
-                                </CardTitle>
-                                <div>
-                                    <center><p>{(typeof props.weather2 != "undefined") && props.weather2.current ? Math.round(props.weather2.daily[5].temp.day) : " error "}&#176;F</p></center>
-                                </div>
-                                <Fade in={props.fade5} className="my-2">
-                                    <CardText>
-                                        <TextForCards cardNum={5} props={props}/>
-                                    </CardText>
-                                </Fade>
-                            </CardBody>
-                        </Card>
-                    </Col>
+                    )
+            }
+        })
+    }
+    return (
+        <div>
+            <Container className="card-container">
+                <Row>
+                    {cardStorage}
+
                 </Row>
             </Container>
         </div>
@@ -173,3 +65,114 @@ const Cards = (props) => {
 
 
 export default Cards;
+
+{/* <Col>
+<Card key="firstCard"
+    tabIndex={0}
+    onClick={() => props.cardOneState()}>
+    <CardBody>
+        <CardTitle>
+            <center>{(typeof props.weather2 != "undefined") && props.weather2.current ? dayConverter(utcConverter(props.weather2.daily[1].dt)) : " error "}</center>
+            <span id="forecast-icon">
+                <center>{(typeof props.weather2 != "undefined") && props.weather2.current ? <img src={`http://openweathermap.org/img/w/${props.weather2.daily[1].weather[0].icon}.png`} /> : " error "}</center>
+            </span>
+        </CardTitle>
+        <div>
+            <center><p>{Math.round(props.weather.main.temp)}&#176;F</p></center>
+        </div>
+        <Fade in={props.fade1} className="my-2">
+            <CardText>
+                <TextForCards cardNum={1} props={props} />
+            </CardText>
+        </Fade>
+    </CardBody>
+</Card>
+</Col>
+<Col>
+<Card key="secondCard"
+    tabIndex={0}
+    onClick={() => props.cardTwoState()}>
+    <CardBody>
+        <CardTitle>
+            <center>{(typeof props.weather2 != "undefined") && props.weather2.current ? dayConverter(utcConverter(props.weather2.daily[2].dt)) : " error "}</center>
+            <span id="forecast-icon">
+                <center>{(typeof props.weather2 != "undefined") && props.weather2.current ? <img src={`http://openweathermap.org/img/w/${props.weather2.daily[2].weather[0].icon}.png`} /> : " error "}</center>
+            </span>
+        </CardTitle>
+        <div>
+            <center><p>{(typeof props.weather2 != "undefined") && props.weather2.current ? Math.round(props.weather2.daily[2].temp.day) : " error "}&#176;F</p></center>
+        </div>
+        <Fade in={props.fade2} className="my-2">
+            <CardText>
+                <TextForCards cardNum={2} props={props}/>
+            </CardText>
+        </Fade>
+    </CardBody>
+</Card>
+</Col>
+<Col>
+<Card key="thirdCard"
+    tabIndex={0}
+    onClick={() => props.cardThreeState()}>
+    <CardBody>
+        <CardTitle>
+            <center>{(typeof props.weather2 != "undefined") && props.weather2.current ? dayConverter(utcConverter(props.weather2.daily[3].dt)) : " error "}</center>
+            <span id="forecast-icon">
+                <center>{(typeof props.weather2 != "undefined") && props.weather2.current ? <img src={`http://openweathermap.org/img/w/${props.weather2.daily[3].weather[0].icon}.png`} /> : " error "}</center>
+            </span>
+        </CardTitle>
+        <div>
+            <center><p>{(typeof props.weather2 != "undefined") && props.weather2.current ? Math.round(props.weather2.daily[3].temp.day) : " error "}&#176;F</p></center>
+        </div>
+        <Fade in={props.fade3} className="my-2">
+            <CardText>
+                <TextForCards cardNum={3} props={props}/>
+            </CardText>
+        </Fade>
+    </CardBody>
+</Card>
+</Col>
+<Col>
+<Card key="fourthCard"
+    tabIndex={0}
+    onClick={() => props.cardFourState()}>
+    <CardBody>
+        <CardTitle>
+            <center>{(typeof props.weather2 != "undefined") && props.weather2.current ? dayConverter(utcConverter(props.weather2.daily[4].dt)) : " error "}</center>
+            <span id="forecast-icon">
+                <center>{(typeof props.weather2 != "undefined") && props.weather2.current ? <img src={`http://openweathermap.org/img/w/${props.weather2.daily[4].weather[0].icon}.png`} /> : " error "}</center>
+            </span>
+        </CardTitle>
+        <div>
+            <center><p>{(typeof props.weather2 != "undefined") && props.weather2.current ? Math.round(props.weather2.daily[4].temp.day) : " error "}&#176;F</p></center>
+        </div>
+        <Fade in={props.fade4} className="my-2">
+            <CardText>
+                <TextForCards cardNum={4} props={props}/>
+            </CardText>
+        </Fade>
+    </CardBody>
+</Card>
+</Col>
+<Col>
+<Card key="fifthCard"
+    tabIndex={0}
+    onClick={() => props.cardFiveState()}>
+    <CardBody>
+        <CardTitle>
+            <center>{(typeof props.weather2 != "undefined") && props.weather2.current ? dayConverter(utcConverter(props.weather2.daily[5].dt)) : " error "}</center>
+            <span id="forecast-icon">
+                <center>{(typeof props.weather2 != "undefined") && props.weather2.current ? <img src={`http://openweathermap.org/img/w/${props.weather2.daily[5].weather[0].icon}.png`} /> : " error "}</center>
+            </span>
+        </CardTitle>
+        <div>
+            <center><p>{(typeof props.weather2 != "undefined") && props.weather2.current ? Math.round(props.weather2.daily[5].temp.day) : " error "}&#176;F</p></center>
+        </div>
+        <Fade in={props.fade5} className="my-2">
+            <CardText>
+                <TextForCards cardNum={5} props={props}/>
+            </CardText>
+        </Fade>
+    </CardBody>
+</Card>
+</Col> */}
--- react_app/src/TextForCards.js
+++ react_app/src/TextForCards.js
@@ -2,9 +2,10 @@ import React from "react";
 
 const TextForCards = (props) =>{
     console.log(props)
+    const {daily} = props
     return(
         <span>
-            Text From card {(typeof props.props.weather2 != "undefined") && props.props.weather2.current ? props.props.weather2.daily[2].temp.day : " error "}
+            {daily.temp.night} 
         </span>
     )
 }
