4447933ec115649a5fbd302eb7b628be378aeaf4
knowdaut <nickdaut975@gmail.com>
2022-03-22 23:06:47 +0000

    added else

--- react_app/src/Cards.js
+++ react_app/src/Cards.js
@@ -32,15 +32,7 @@ const Cards = (props) => {
     const [isOpen3, setIsOpen3] = useState(false);
     const [isOpen4, setIsOpen4] = useState(false);
     const [isOpen5, setIsOpen5] = useState(false);
-    // const [setActive, setActiveState] = useState("active");
-    // const [setRotate, setRotateState] = useState("arrow");
 
-    // function rotateArrow(){
-    //     setActiveState(setActive === "" ? "active" : "") 
-    //     setRotateState(
-    //         setActive === "active" ? "rotate-arrow" : "arrow"
-    //     );
-    // }
     if (typeof props.weather2 != "undefined" && props.weather2.daily) {
         return (
             <div>
@@ -52,7 +44,6 @@ const Cards = (props) => {
                                     <center>
                                     <CardTitle>
                                         {dayConverter(utcConverter(props.weather2.daily[1].dt))}
-                                        {/* <Chevron className={`${setRotate}`} width={7} fill={"#777"} /> */}
                                     </CardTitle>
                                     <CardTitle>
                                         {<img src={`http://openweathermap.org/img/w/${props.weather2.daily[1].weather[0].icon}.png`} />}
@@ -64,7 +55,6 @@ const Cards = (props) => {
                                             <p>{toTitleCase(props.weather2.daily[1].weather[0].description)}</p>
                                             <p>Low: {Math.round(props.weather2.daily[1].temp.min)}&#176;F</p>
                                             <p>High: {Math.round(props.weather2.daily[1].temp.max)}&#176;F</p>
-                                            {/* <TextForCards cardNum={1} props={props} /> */}
                                         </CardText>
                                         </center>
                                     </Collapse>
@@ -78,7 +68,6 @@ const Cards = (props) => {
                                     <CardTitle>
                                         {dayConverter(utcConverter(props.weather2.daily[2].dt))}
                                                                       
-                                        {/* <Chevron className={`${setRotate}`} width={7} fill={"#777"} /> */}
                                     </CardTitle>
                                     <CardTitle>
                                         {<img src={`http://openweathermap.org/img/w/${props.weather2.daily[2].weather[0].icon}.png`} />}
@@ -91,7 +80,6 @@ const Cards = (props) => {
                                             <p>Low: {Math.round(props.weather2.daily[2].temp.min)}&#176;F</p>
                                             <p>High: {Math.round(props.weather2.daily[2].temp.max)}&#176;F</p>
     
-                                            {/* <TextForCards cardNum={2} props={props}/> */}
                                         </CardText>
                                         </center>
                                     </Collapse>
@@ -104,7 +92,6 @@ const Cards = (props) => {
                                     <center>
                                     <CardTitle>
                                         {dayConverter(utcConverter(props.weather2.daily[3].dt))}
-                                        {/* <Chevron className={`${setRotate}`} width={7} fill={"#777"} /> */}
                                     </CardTitle>
                                     <CardTitle>
                                         {<img src={`http://openweathermap.org/img/w/${props.weather2.daily[3].weather[0].icon}.png`} />}
@@ -117,7 +104,6 @@ const Cards = (props) => {
                                             <p>Low: {Math.round(props.weather2.daily[3].temp.min)}&#176;F</p>
                                             <p>High: {Math.round(props.weather2.daily[3].temp.max)}&#176;F</p>
     
-                                            {/* <TextForCards cardNum={3} props={props}/> */}
                                         </CardText>
                                         </center>
                                     </Collapse>
@@ -130,7 +116,6 @@ const Cards = (props) => {
                                     <center>
                                     <CardTitle>
                                         {dayConverter(utcConverter(props.weather2.daily[4].dt))}
-                                        {/* <Chevron className={`${setRotate}`} width={7} fill={"#777"} /> */}
                                     </CardTitle>
                                     <CardTitle>
                                         {<img src={`http://openweathermap.org/img/w/${props.weather2.daily[4].weather[0].icon}.png`} />}
@@ -142,7 +127,6 @@ const Cards = (props) => {
                                             <p>{toTitleCase(props.weather2.daily[4].weather[0].description)}</p>
                                             <p>Low: {Math.round(props.weather2.daily[4].temp.min) }&#176;F</p>
                                             <p>High: {Math.round(props.weather2.daily[4].temp.max)}&#176;F</p>
-                                            {/* <TextForCards cardNum={4} props={props}/> */}
                                         </CardText>
                                         </center>
                                     </Collapse>
@@ -155,7 +139,6 @@ const Cards = (props) => {
                                     <center>
                                     <CardTitle>
                                         {dayConverter(utcConverter(props.weather2.daily[5].dt))}
-                                        {/* <Chevron className={`${setRotate}`} width={7} fill={"#777"} /> */}
                                     </CardTitle>
                                     <CardTitle>
                                         {<img src={`http://openweathermap.org/img/w/${props.weather2.daily[5].weather[0].icon}.png`} />}
@@ -167,7 +150,6 @@ const Cards = (props) => {
                                             <p>{toTitleCase(props.weather2.daily[5].weather[0].description)}</p>
                                             <p>Low: { Math.round(props.weather2.daily[5].temp.min)}&#176;F</p>
                                             <p>High: {Math.round(props.weather2.daily[5].temp.max)}&#176;F</p>
-                                            {/* <TextForCards cardNum={5} props={props}/> */}
                                         </CardText>
                                         </center>
                                     </Collapse>
@@ -181,7 +163,117 @@ const Cards = (props) => {
     }
     else{
         return(
-            <p>error</p>
+            <div>
+                <Container className="card-container">
+                    <Col>
+                        <Row>
+                            <Card key="firstCard" tabIndex={0} onKeyPress={(evt) => { if (evt.key === "Enter") { setIsOpen1(!isOpen1) } }} onClick={() => { setIsOpen1(!isOpen1)}}>
+                                <CardBody>
+                                    <center>
+                                    <CardTitle>
+                                    <p>day of week</p>
+                                    </CardTitle>
+                                    <CardTitle>
+                                    <p>image error</p>
+                                    </CardTitle>
+                                    </center>
+                                    <Collapse isOpen={isOpen1}>
+                                        <center>
+                                        <CardText>
+                                            <p>data</p>
+                                        </CardText>
+                                        </center>
+                                    </Collapse>
+                                </CardBody>
+                            </Card>
+                        </Row>
+                        <Row>
+                            <Card key="secondCard" tabIndex={0} onKeyPress={(evt) => { if (evt.key === "Enter") { setIsOpen2(!isOpen2) } }} onClick={() => { setIsOpen2(!isOpen2)}}>
+                                <CardBody>
+                                    <center>
+                                    <CardTitle>
+                                        <p>day of week</p>                                 
+                                    </CardTitle>
+                                    <CardTitle>
+                                        <p>image error</p>
+                                    </CardTitle>
+                                    </center>
+                                    <Collapse isOpen={isOpen2}>
+                                        <center>
+                                        <CardText>
+                                            <p>data</p>
+                                        </CardText>
+                                        </center>
+                                    </Collapse>
+                                </CardBody>
+                            </Card>
+                        </Row>
+                        <Row>
+                            <Card key="thirdCard" tabIndex={0} onKeyPress={(evt) => { if (evt.key === "Enter") { setIsOpen3(!isOpen3) } }}onClick={() => { setIsOpen3(!isOpen3)}}>
+                                <CardBody>
+                                    <center>
+                                    <CardTitle>
+                                        <p>day of week</p>
+                                    </CardTitle>
+                                    <CardTitle>
+                                        <p>image error</p>
+                                    </CardTitle>
+                                    </center>
+                                    <Collapse isOpen={isOpen3}>
+                                        <center>
+                                        <CardText>
+                                            <p>data</p>
+                                        </CardText>
+                                        </center>
+                                    </Collapse>
+                                </CardBody>
+                            </Card>
+                        </Row>
+                        <Row>
+                            <Card key="fourthCard" tabIndex={0} onKeyPress={(evt) => { if (evt.key === "Enter") { setIsOpen4(!isOpen4) } }} onClick={() => { setIsOpen4(!isOpen4)}}>
+                                <CardBody>
+                                    <center>
+                                    <CardTitle>
+                                        <p>day of week</p>
+                                    </CardTitle>
+                                    <CardTitle>
+                                    <p>image error</p>
+                                    </CardTitle>
+                                    </center>
+                                    <Collapse isOpen={isOpen4}>
+                                        <center>
+                                        <CardText>
+                                            <p>data</p>
+                                        </CardText>
+                                        </center>
+                                    </Collapse>
+                                </CardBody>
+                            </Card>
+                        </Row>
+                        <Row>
+                            <Card key="fifthCard" tabIndex={0} onKeyPress={(evt) => { if (evt.key === "Enter") { setIsOpen5(!isOpen5) } }} onClick={() => { setIsOpen5(!isOpen5)}}>
+                                <CardBody>
+                                    <center>
+                                    <CardTitle>
+                                        <p>day of week</p>
+                                    </CardTitle>
+                                    <CardTitle>
+                                        <p>image error</p>
+                                    </CardTitle>
+                                    </center>
+                                    <Collapse isOpen={isOpen5}>
+                                        <center>
+                                        <CardText>
+                                            <p>data</p>
+                                        </CardText>
+                                        </center>
+                                    </Collapse>
+                                </CardBody>
+                            </Card>
+                        </Row>
+                    </Col>
+                </Container>
+            </div>
         )
     }
 
@@ -190,37 +282,3 @@ const Cards = (props) => {
 
 
 export default Cards;
-
-// if (typeof props.weather2 != "undefined" && props.weather2.daily) {
-    //     props.weather2.daily.forEach((daily, index) => {
-    //         if (index > 0 && index < 6) {
-    //             cardStorage.push(
-    //                 <Col>
-    //                     <Card key="firstCard"
-    //                         tabIndex={0}
-    //                         onClick={() => props.cardOneState()}>
-    //                         <CardBody>
-    //                             <CardTitle>
-    //                                  {dayConverter(utcConverter(daily.dt))}
-    //                                 <span id="forecast-icon">
-    //                                      {(typeof props.weather2 != "undefined") && props.weather2.current ? <img src={`http://openweathermap.org/img/w/${daily.weather[0].icon}.png`} /> : " "}
-    //                                 </span>
-    //                             </CardTitle>
-    //                             <div>
-    //
-    //                                       {toTitleCase(daily.weather[0].description)}
-    //                                       {Math.round(daily.temp.day)}&#176;F
-    //
-    //                             </div>
-    //                             <Collapse in={props.Collapse1} className="my-2">
-    //                                 <CardText>
-    //                                     <TextForCards cardNum={1} daily={daily} />
-    //                                 </CardText>
-    //                             </Collapse>
-    //                         </CardBody>
-    //                     </Card>
-    //                 </Col>
-    //             )
-    //         }
-    //     })
-    // }
