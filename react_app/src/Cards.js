6233972c6df10b7a1aa0f2943983e2ee348a773a
knowdaut <nickdaut975@gmail.com>
2022-03-22 22:03:48 +0000

    fixed ternary issues

--- react_app/src/Cards.js
+++ react_app/src/Cards.js
@@ -41,133 +41,150 @@ const Cards = (props) => {
     //         setActive === "active" ? "rotate-arrow" : "arrow"
     //     );
     // }
+    if (typeof props.weather2 != "undefined" && props.weather2.daily) {
+        return (
+            <div>
+                <Container className="card-container">
+                    <Col>
+                        <Row>
+                            <Card key="firstCard" tabIndex={0} onKeyPress={(evt) => { if (evt.key === "Enter") { setIsOpen1(!isOpen1) } }} onClick={() => { setIsOpen1(!isOpen1)}}>
+                                <CardBody>
+                                    <center>
+                                    <CardTitle>
+                                        {dayConverter(utcConverter(props.weather2.daily[1].dt))}
+                                        {/* <Chevron className={`${setRotate}`} width={7} fill={"#777"} /> */}
+                                    </CardTitle>
+                                    <CardTitle>
+                                        {<img src={`http://openweathermap.org/img/w/${props.weather2.daily[1].weather[0].icon}.png`} />}
+                                    </CardTitle>
+                                    </center>
+                                    <Collapse isOpen={isOpen1}>
+                                        <center>
+                                        <CardText>
+                                            <p>{toTitleCase(props.weather2.daily[1].weather[0].description)}</p>
+                                            <p>Low: {Math.round(props.weather2.daily[1].temp.min)}&#176;F</p>
+                                            <p>High: {Math.round(props.weather2.daily[1].temp.max)}&#176;F</p>
+                                            {/* <TextForCards cardNum={1} props={props} /> */}
+                                        </CardText>
+                                        </center>
+                                    </Collapse>
+                                </CardBody>
+                            </Card>
+                        </Row>
+                        <Row>
+                            <Card key="secondCard" tabIndex={0} onKeyPress={(evt) => { if (evt.key === "Enter") { setIsOpen2(!isOpen2) } }} onClick={() => { setIsOpen2(!isOpen2)}}>
+                                <CardBody>
+                                    <center>
+                                    <CardTitle>
+                                        {dayConverter(utcConverter(props.weather2.daily[2].dt))}
+                                                                      
+                                        {/* <Chevron className={`${setRotate}`} width={7} fill={"#777"} /> */}
+                                    </CardTitle>
+                                    <CardTitle>
+                                        {<img src={`http://openweathermap.org/img/w/${props.weather2.daily[2].weather[0].icon}.png`} />}
+                                    </CardTitle>
+                                    </center>
+                                    <Collapse isOpen={isOpen2}>
+                                        <center>
+                                        <CardText>
+                                            <p>{toTitleCase(props.weather2.daily[2].weather[0].description)}</p>
+                                            <p>Low: {Math.round(props.weather2.daily[2].temp.min)}&#176;F</p>
+                                            <p>High: {Math.round(props.weather2.daily[2].temp.max)}&#176;F</p>
+    
+                                            {/* <TextForCards cardNum={2} props={props}/> */}
+                                        </CardText>
+                                        </center>
+                                    </Collapse>
+                                </CardBody>
+                            </Card>
+                        </Row>
+                        <Row>
+                            <Card key="thirdCard" tabIndex={0} onKeyPress={(evt) => { if (evt.key === "Enter") { setIsOpen3(!isOpen3) } }}onClick={() => { setIsOpen3(!isOpen3)}}>
+                                <CardBody>
+                                    <center>
+                                    <CardTitle>
+                                        {dayConverter(utcConverter(props.weather2.daily[3].dt))}
+                                        {/* <Chevron className={`${setRotate}`} width={7} fill={"#777"} /> */}
+                                    </CardTitle>
+                                    <CardTitle>
+                                        {<img src={`http://openweathermap.org/img/w/${props.weather2.daily[3].weather[0].icon}.png`} />}
+                                    </CardTitle>
+                                    </center>
+                                    <Collapse isOpen={isOpen3}>
+                                        <center>
+                                        <CardText>
+                                            <p>{toTitleCase(props.weather2.daily[3].weather[0].description)}</p>
+                                            <p>Low: {Math.round(props.weather2.daily[3].temp.min)}&#176;F</p>
+                                            <p>High: {Math.round(props.weather2.daily[3].temp.max)}&#176;F</p>
+    
+                                            {/* <TextForCards cardNum={3} props={props}/> */}
+                                        </CardText>
+                                        </center>
+                                    </Collapse>
+                                </CardBody>
+                            </Card>
+                        </Row>
+                        <Row>
+                            <Card key="fourthCard" tabIndex={0} onKeyPress={(evt) => { if (evt.key === "Enter") { setIsOpen4(!isOpen4) } }} onClick={() => { setIsOpen4(!isOpen4)}}>
+                                <CardBody>
+                                    <center>
+                                    <CardTitle>
+                                        {dayConverter(utcConverter(props.weather2.daily[4].dt))}
+                                        {/* <Chevron className={`${setRotate}`} width={7} fill={"#777"} /> */}
+                                    </CardTitle>
+                                    <CardTitle>
+                                        {<img src={`http://openweathermap.org/img/w/${props.weather2.daily[4].weather[0].icon}.png`} />}
+                                    </CardTitle>
+                                    </center>
+                                    <Collapse isOpen={isOpen4}>
+                                        <center>
+                                        <CardText>
+                                            <p>{toTitleCase(props.weather2.daily[4].weather[0].description)}</p>
+                                            <p>Low: {Math.round(props.weather2.daily[4].temp.min) }&#176;F</p>
+                                            <p>High: {Math.round(props.weather2.daily[4].temp.max)}&#176;F</p>
+                                            {/* <TextForCards cardNum={4} props={props}/> */}
+                                        </CardText>
+                                        </center>
+                                    </Collapse>
+                                </CardBody>
+                            </Card>
+                        </Row>
+                        <Row>
+                            <Card key="fifthCard" tabIndex={0} onKeyPress={(evt) => { if (evt.key === "Enter") { setIsOpen5(!isOpen5) } }} onClick={() => { setIsOpen5(!isOpen5)}}>
+                                <CardBody>
+                                    <center>
+                                    <CardTitle>
+                                        {dayConverter(utcConverter(props.weather2.daily[5].dt))}
+                                        {/* <Chevron className={`${setRotate}`} width={7} fill={"#777"} /> */}
+                                    </CardTitle>
+                                    <CardTitle>
+                                        {<img src={`http://openweathermap.org/img/w/${props.weather2.daily[5].weather[0].icon}.png`} />}
+                                    </CardTitle>
+                                    </center>
+                                    <Collapse isOpen={isOpen5}>
+                                        <center>
+                                        <CardText>
+                                            <p>{toTitleCase(props.weather2.daily[5].weather[0].description)}</p>
+                                            <p>Low: { Math.round(props.weather2.daily[5].temp.min)}&#176;F</p>
+                                            <p>High: {Math.round(props.weather2.daily[5].temp.max)}&#176;F</p>
+                                            {/* <TextForCards cardNum={5} props={props}/> */}
+                                        </CardText>
+                                        </center>
+                                    </Collapse>
+                                </CardBody>
+                            </Card>
+                        </Row>
+                    </Col>
+                </Container>
+            </div>
+        )
+    }
+    else{
+        return(
+            <p>error</p>
+        )
+    }
 
-    return (
-        <div>
-            <Container className="card-container">
-                <Col>
-                    <Row>
-                        <Card key="firstCard" tabIndex={0} onKeyPress={(evt) => { if (evt.key === "Enter") { setIsOpen1(!isOpen1) } }} onClick={() => { setIsOpen1(!isOpen1)}}>
-                            <CardBody>
-                                <center>
-                                <CardTitle>
-                                    {(typeof props.weather2 != "undefined") && props.weather2.current ? dayConverter(utcConverter(props.weather2.daily[1].dt)) : " "}
-                                    {/* <Chevron className={`${setRotate}`} width={7} fill={"#777"} /> */}
-                                </CardTitle>
-                                </center>
-                                <Collapse isOpen={isOpen1}>
-                                    <center>
-                                    <CardText>
-                                        {(typeof props.weather2 != "undefined") && props.weather2.current ? <img src={`http://openweathermap.org/img/w/${props.weather2.daily[1].weather[0].icon}.png`} /> : " "}
-                                        <p>{(typeof props.weather2 != "undefined") && props.weather2.current ? toTitleCase(props.weather2.daily[1].weather[0].description) : " "}</p>
-                                        <p>Low: {(typeof props.weather2 != "undefined") && props.weather2.current ? Math.round(props.weather2.daily[1].temp.min) : " "}&#176;F</p>
-                                        <p>High: {(typeof props.weather2 != "undefined") && props.weather2.current ? Math.round(props.weather2.daily[1].temp.max) : " "}&#176;F</p>
-                                        {/* <TextForCards cardNum={1} props={props} /> */}
-                                    </CardText>
-                                    </center>
-                                </Collapse>
-                            </CardBody>
-                        </Card>
-                    </Row>
-                    <Row>
-                        <Card key="secondCard" tabIndex={0} onKeyPress={(evt) => { if (evt.key === "Enter") { setIsOpen2(!isOpen2) } }} onClick={() => { setIsOpen2(!isOpen2)}}>
-                            <CardBody>
-                                <center>
-                                <CardTitle>
-                                    {(typeof props.weather2 != "undefined") && props.weather2.current ? dayConverter(utcConverter(props.weather2.daily[2].dt)) : " "}
-                                                                  
-                                    {/* <Chevron className={`${setRotate}`} width={7} fill={"#777"} /> */}
-                                </CardTitle>
-                                </center>
-                                <Collapse isOpen={isOpen2}>
-                                    <center>
-                                    <CardText>
-                                        {(typeof props.weather2 != "undefined") && props.weather2.current ? <img src={`http://openweathermap.org/img/w/${props.weather2.daily[2].weather[0].icon}.png`} /> : " "}
-                                        <p>{(typeof props.weather2 != "undefined") && props.weather2.current ? toTitleCase(props.weather2.daily[2].weather[0].description) : " "}</p>
-                                        <p>Low: {(typeof props.weather2 != "undefined") && props.weather2.current ? Math.round(props.weather2.daily[2].temp.min) : " "}&#176;F</p>
-                                        <p>High: {(typeof props.weather2 != "undefined") && props.weather2.current ? Math.round(props.weather2.daily[2].temp.max) : " "}&#176;F</p>
-
-                                        {/* <TextForCards cardNum={2} props={props}/> */}
-                                    </CardText>
-                                    </center>
-                                </Collapse>
-                            </CardBody>
-                        </Card>
-                    </Row>
-                    <Row>
-                        <Card key="thirdCard" tabIndex={0} onKeyPress={(evt) => { if (evt.key === "Enter") { setIsOpen3(!isOpen3) } }}onClick={() => { setIsOpen3(!isOpen3)}}>
-                            <CardBody>
-                                <center>
-                                <CardTitle>
-                                    {(typeof props.weather2 != "undefined") && props.weather2.current ? dayConverter(utcConverter(props.weather2.daily[3].dt)) : " "}
-                                    {/* <Chevron className={`${setRotate}`} width={7} fill={"#777"} /> */}
-                                </CardTitle>
-                                </center>
-                                <Collapse isOpen={isOpen3}>
-                                    <center>
-                                    <CardText>
-                                        {(typeof props.weather2 != "undefined") && props.weather2.current ? <img src={`http://openweathermap.org/img/w/${props.weather2.daily[3].weather[0].icon}.png`} /> : " "}
-                                        <p>{(typeof props.weather2 != "undefined") && props.weather2.current ? toTitleCase(props.weather2.daily[3].weather[0].description) : " "}</p>
-                                        <p>Low: {(typeof props.weather2 != "undefined") && props.weather2.current ? Math.round(props.weather2.daily[3].temp.min) : " "}&#176;F</p>
-                                        <p>High: {(typeof props.weather2 != "undefined") && props.weather2.current ? Math.round(props.weather2.daily[3].temp.max) : " "}&#176;F</p>
-
-                                        {/* <TextForCards cardNum={3} props={props}/> */}
-                                    </CardText>
-                                    </center>
-                                </Collapse>
-                            </CardBody>
-                        </Card>
-                    </Row>
-                    <Row>
-                        <Card key="fourthCard" tabIndex={0} onKeyPress={(evt) => { if (evt.key === "Enter") { setIsOpen4(!isOpen4) } }} onClick={() => { setIsOpen4(!isOpen4)}}>
-                            <CardBody>
-                                <center>
-                                <CardTitle>
-                                    {(typeof props.weather2 != "undefined") && props.weather2.current ? dayConverter(utcConverter(props.weather2.daily[4].dt)) : " "}
-                                    {/* <Chevron className={`${setRotate}`} width={7} fill={"#777"} /> */}
-                                </CardTitle>
-                                </center>
-                                <Collapse isOpen={isOpen4}>
-                                    <center>
-                                    <CardText>
-                                        {(typeof props.weather2 != "undefined") && props.weather2.current ? <img src={`http://openweathermap.org/img/w/${props.weather2.daily[4].weather[0].icon}.png`} /> : " "}
-                                        <p>{(typeof props.weather2 != "undefined") && props.weather2.current ? toTitleCase(props.weather2.daily[4].weather[0].description) : " "}</p>
-                                        <p>Low: {(typeof props.weather2 != "undefined") && props.weather2.current ? Math.round(props.weather2.daily[4].temp.min) : " "}&#176;F</p>
-                                        <p>High: {(typeof props.weather2 != "undefined") && props.weather2.current ? Math.round(props.weather2.daily[4].temp.max) : " "}&#176;F</p>
-                                        {/* <TextForCards cardNum={4} props={props}/> */}
-                                    </CardText>
-                                    </center>
-                                </Collapse>
-                            </CardBody>
-                        </Card>
-                    </Row>
-                    <Row>
-                        <Card key="fifthCard" tabIndex={0} onKeyPress={(evt) => { if (evt.key === "Enter") { setIsOpen5(!isOpen5) } }} onClick={() => { setIsOpen5(!isOpen5)}}>
-                            <CardBody>
-                                <center>
-                                <CardTitle>
-                                    {(typeof props.weather2 != "undefined") && props.weather2.current ? dayConverter(utcConverter(props.weather2.daily[5].dt)) : " "}
-                                    {/* <Chevron className={`${setRotate}`} width={7} fill={"#777"} /> */}
-                                </CardTitle>
-                                </center>
-                                <Collapse isOpen={isOpen5}>
-                                    <center>
-                                    <CardText>
-                                        {(typeof props.weather2 != "undefined") && props.weather2.current ? <img src={`http://openweathermap.org/img/w/${props.weather2.daily[5].weather[0].icon}.png`} /> : " "}
-                                        <p>{(typeof props.weather2 != "undefined") && props.weather2.current ? toTitleCase(props.weather2.daily[5].weather[0].description) : " "}</p>
-                                        <p>Low: {(typeof props.weather2 != "undefined") && props.weather2.current ? Math.round(props.weather2.daily[5].temp.min) : " "}&#176;F</p>
-                                        <p>High: {(typeof props.weather2 != "undefined") && props.weather2.current ? Math.round(props.weather2.daily[5].temp.max) : " "}&#176;F</p>
-                                        {/* <TextForCards cardNum={5} props={props}/> */}
-                                    </CardText>
-                                    </center>
-                                </Collapse>
-                            </CardBody>
-                        </Card>
-                    </Row>
-                </Col>
-            </Container>
-        </div>
-    )
 }
 
 
