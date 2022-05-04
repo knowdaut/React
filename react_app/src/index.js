8acd1b2419567efac59303e1008dfbd567016422
tylerkwallace <54109041+tylerkwallace@users.noreply.github.com>
2022-05-04 10:33:52 -0500

    Removed commented out code

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -7,20 +7,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './CSS/App.css';
 
 
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-
 export default ReactDOM.render(
   <App />,
   document.getElementById('root') || document.createElement('div'),
 );
 
-
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
