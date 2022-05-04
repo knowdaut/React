01c0f3319b285919a0ed760ffe5d47e8dddd99da
tylerkwallace <54109041+tylerkwallace@users.noreply.github.com>
2022-05-04 10:32:34 -0500

    Removed commented out code

--- react_app/src/__tests__/IndCards.test.js
+++ react_app/src/__tests__/IndCards.test.js
@@ -3,39 +3,8 @@ import IndCard from '../Components/IndCard';
 import React from "react";
 import userEvent from "@testing-library/user-event";
 
-// const weatherCurrent = {
-//     base: "stations",
-//     clouds: {all: 0},
-//     cod: 200,
-//     coord: {lon: -84.388, lat: 33.749},
-//     dt: 1648594881,
-//     id: 4180439,
-//     main: {temp: 73, feels_like: 71.83, temp_min: 68.77, temp_max: 75.58, pressure: 1017, humidity: 39},
-//     name: "Atlanta",
-//     sys: {type: 2, id: 2006620, country: "US", sunrise: 1648553335, sunset: 1648598124},
-//     timezone: -14400,
-//     visibility: 10000,
-//     weather: [{id: 800, main: "Clear", description: "clear sky", icon: "01d"}],
-//     wind: {speed: 6.91, deg: 140}
-// }
 
 const daily =
-// current: {
-//     dt: 1648595317, 
-//     sunrise: 1648553335, 
-//     sunset: 1648598124, 
-//     temp: 72.64, 
-//     feels_like: 71.44,
-//     clouds: 0,
-//     dew_point: 46.29,
-//     humidity: 39,
-//     pressure: 1018,
-//     uvi: 0.21,
-//     visibility: 10000,
-//     weather: [{id: 800, main: "Clear", description: "clear sky", icon: "01d"}],
-//     wind_deg: 140,
-//     wind_speed: 8.05
-// },
 {
   clouds: 80,
   dew_point: 35.22,
