786cc1c7e7235ccd199a1e1752657e1ceb0e8b4d
knowdaut <71151073+knowdaut@users.noreply.github.com>
2022-05-08 00:42:44 -0400

    Update Cards.js

--- react_app/src/Cards.js
+++ react_app/src/Cards.js
@@ -1,41 +1,19 @@
-import React, {useState} from "react";
-import { Collapse, Card, CardBody, CardText, CardTitle, Container, Row, Col } from 'reactstrap';
-// import Chevron from "./Chevron.js";
+import React from "react";
+import {Container} from 'reactstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./Cards.css";
+import "../CSS/Cards.css";
 import IndCard from "./IndCard";
-import TextForCards from "./TextForCards";
-
-function utcConverter(utcTime) {
-    var d = new Date(0);
-    d.setUTCSeconds(utcTime)
-    var d2 = d.getDay()
-    return d2;
-}
-
-
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-function dayConverter(dayNum) {
-    console.log(days[dayNum])
-    return days[dayNum]
-}
-
-function toTitleCase(str) {
-    return str.replace(/\w\S*/g, function (txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
-}
+import {utcConverter, dayConverter} from '../Utils/Converters.js';
 
 
 const Cards = (props) => {
-    console.log(props)
     const cardStorage = []
 
-    if (typeof props.weather2 != "undefined" && props.weather2.daily) {
-        props.weather2.daily.forEach((daily, index) => {
+    if (typeof props.weatherDaily != "undefined" && props.weatherDaily.daily) {
+        props.weatherDaily.daily.forEach((daily, index) => {
             if (index > 0 && index < 6) {
                 cardStorage.push(
-                    <IndCard key={index} utcConverter={utcConverter} dayConverter={dayConverter} daily={daily} toTitleCase={toTitleCase}/>
+                    <IndCard key={index} utcConverter={utcConverter} dayConverter={dayConverter} daily={daily} />
                 )
             }
         })
